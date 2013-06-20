abcd6b7c6fb291eb64058c4b195c245aaac63458
Kylin <kylinsoong.1214@gmail.com>
2013-06-20 18:04:39 +0800

    add spring jboss integeration code

---
+++ helloworld/src/main/java/com/jcommerce/core/model/KylinSoong.java
@@ -0,0 +1,57 @@
+package com.jcommerce.core.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "kylinsoong")
+public class KylinSoong implements Serializable {
+
+	private static final long serialVersionUID = 6027058544242203518L;
+
+	private String id;
+	
+	private String name;
+	
+	public KylinSoong() {
+		
+	}
+
+	public KylinSoong(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	@Id
+	@Basic(optional = false)
+	@GeneratedValue
+	@Column(name = "id", nullable = false, length = 32)
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "KylinSoong [id=" + id + ", name=" + name + "]";
+	}
+
+}
---
+++ travel/src/main/java/org/springframework/samples/travel/User.java
@@ -0,0 +1,72 @@
+package org.springframework.samples.travel;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * A user who can book hotels.
+ */
+@Entity
+@Table(name = "Customer")
+public class User implements Serializable {
+	
+	private Long id;
+
+	private String username;
+
+	private String password;
+
+	private String name;
+
+	public User() {
+	}
+
+	public User(String username, String password, String name) {
+		this.username = username;
+		this.password = password;
+		this.name = name;
+	}
+
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "User(" + username + ")";
+	}
+}
